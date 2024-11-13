6957ecf50fa4ebd5f345826f56664a0f9919ebf1
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-11-14 00:59:04 +0600

    solve

--- Sieve, Prime Factorization and Divisors/Primal_Fear.cpp
+++ Sieve, Prime Factorization and Divisors/Primal_Fear.cpp
@@ -1,94 +1,32 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 14-11-2024
+ * time : 00:07:09
+ * Problem Name : Primal_Fear
+ *
+ **/
+#ifdef __GNUC__
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#endif
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long int;
-
-const int lm = 1e7 + 123;
-bitset<lm> ip;
-vector<int> p(lm, 0);
-
-// Truncatable prime চেক করা
-bool isTruncatablePrime(int n)
-{
-    int divisor = 10;
-    while (divisor <= n)
-    {
-        if (!ip[n % divisor])
-        {
-            return false;
-        }
-        divisor *= 10;
-    }
-    return true;
-}
-
-// Sieve of Eratosthenes
-void sieve(int n)
-{
-    ip[2] = true;
-    for (int i = 3; i <= n; i += 2)
-    {
-        ip[i] = true;
-    }
-    for (int i = 3; i * i <= n; i += 2)
-    {
-        if (ip[i])
-        {
-            for (int j = i * i; j <= n; j += i + i)
-            {
-                ip[j] = false;
-            }
-        }
-    }
-
-    for (int i = 2; i <= n; i++)
-    {
-        if (ip[i])
-        {
-            int temp = i;
-            bool hasZero = false;
-
-            // Check if the number has digit 0
-            while (temp > 0)
-            {
-                if (temp % 10 == 0)
-                {
-                    hasZero = true;
-                    break;
-                }
-                temp /= 10;
-            }
-
-            // যদি 0 না থাকে এবং truncatable prime হয়
-            if (!hasZero && isTruncatablePrime(i))
-            {
-                p[i] = p[i - 1] + 1;
-            }
-            else
-            {
-                p[i] = p[i - 1];
-            }
-        }
-        else
-        {
-            p[i] = p[i - 1];
-        }
-    }
-}
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+#define mem(dp, i) memset(dp, i, sizeof(dp));
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
-    sieve(lm);
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int v;
-        cin >> v;
-        cout << p[v] << endl;
-    }
     return 0;
 }
